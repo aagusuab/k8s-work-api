030953742d1dbfab25da3c5cadc191b29fb5ce74
Youn Jae Kim <younjaekim@microsoft.com>
2022-07-14 14:47:19 -0700

    import fix

--- tests/e2e/apply_test.go
+++ tests/e2e/apply_test.go
@@ -2,11 +2,11 @@ package e2e
 
 import (
 	"context"
-	"k8s.io/apimachinery/pkg/api/meta"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
